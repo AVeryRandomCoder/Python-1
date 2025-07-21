f03c4b688a5f8b79fd35d281e9647667d37dc72c
AVeryRandomAccount <108857697+AVeryRandomCoder@users.noreply.github.com>
2025-07-21 19:41:25 +0700

    Add files via upload

---
+++ L1-A1.py
@@ -0,0 +1,9 @@
+# This is Pycharm, better for python I think
+print("Hello World!")
+
+print("My name is Edwin")
+
+print("And I made the mimic")
+
+print("And the result of 3+5 is: ", 3+5)
+
---
+++ L1-A2.py
@@ -0,0 +1,8 @@
+print("Welcome to the world of Go- I mean programming")
+
+print(4)
+
+print("Hello Python \n \n \nThis sentence is coming in a new line")
+
+import keyword
+print(keyword.kwlist)
